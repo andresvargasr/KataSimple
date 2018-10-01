dd6ef2e59b3b5aaf53f4964198e139a439f7219a
ja.martinezl <ja.martinezl@uniandes.edu.co>
2018-09-30 21:24:54 -0500

    Paso 2 Ciclo 4: Se modifica el metodo demo para retornar el promedio de la cadena de entrada (Verde)

--- Solver.py
+++ Solver.py
@@ -5,9 +5,11 @@ class Solver:
         result = [0, 0, 0, 0]
         min_num = 999
         max_num = 0
+        suma=0
         split_numbers = numbers.strip().replace(" ", "").split(',')
         for x in split_numbers:
             if x.isdigit():
+                suma+=int(x)
                 if int(x) < int(min_num):
                     min_num = x
                     result[1] = int(min_num)
@@ -15,6 +17,7 @@ class Solver:
                     max_num = x
                     result[2] = int(max_num)
                 result[0] = result[0] + 1
+                result[3]=suma/int(result[0])
 
         return result
 
