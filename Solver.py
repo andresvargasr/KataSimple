877d1ad86723ef2a6ad5e8d76b45fa8d002091c6
GLOBANT\andres.vargas <andres.vargas@globant.com>
2018-09-30 20:29:55 -0500

    (VERDE) 1. Se escribe el codigo necesario para devolver un arreglo con el número de elementos

--- Solver.py
+++ Solver.py
@@ -3,6 +3,11 @@ class Solver:
     def demo(self, numbers):
 
         result = [0, 0, 0, 0]
+        split_numbers = numbers.replace(" ", "").split(',')
+        for x in split_numbers:
+            if x.isdigit():
+                print(x)
+                result[0] = result[0] + 1
         return result
 
 Solver().demo("1,2,4,6,7,8")
