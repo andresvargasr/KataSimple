b8d69cb6b1b72e9c690e709f1d8bef65ba0c2119
ja.martinezl <ja.martinezl@uniandes.edu.co>
2018-09-30 21:04:21 -0500

    Paso 1 Ciclo 2: Se modifica el metodo demo para retornar el valor minimo de la cadena de entrada (Verde)

--- Solver.py
+++ Solver.py
@@ -3,12 +3,16 @@ class Solver:
     def demo(self, numbers):
 
         result = [0, 0, 0, 0]
+        min=999
 
         split_numbers = numbers.strip().replace(" ", "").split(',')
         for x in split_numbers:
             if x.isdigit():
-                print(x)
+                if int(x)<int(min):
+                    min=x
+                    result[1] = int(min)
                 result[0] = result[0] + 1
+
         return result
 
 Solver().demo("1,2,4,6,7,8")
